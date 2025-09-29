b76e25e361c35e1ae6091f38f0a6bb0861671026
sumitkshaw <shawsumit6286@gmail.com>
2025-09-29 14:57:03 +0530

    rabbitmq-implemented

--- task-service/index.js
+++ task-service/index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser'); // helps request json request body
+const amqp = require('amqplib');
 
 const app = express() // we are creating an exp app
 const port = 3002 // this is the variable having port number where our application will work
@@ -24,6 +25,25 @@ const TaskSchema = new mongoose.Schema({
     }
 });
 const Task = mongoose.model('Task', TaskSchema);
+
+let  channel, connection;
+
+async function connectRabbitMQWithRetry(retries=5, delay=3000){
+    while(retries){
+        try {
+            connection = await amqp.connect("amqp://rabbitmq_node");
+            channel = await connection.createChannel();
+            await channel.assertQueue("task_created");
+            console.log("Connected to RabbitMQ");
+            return;
+        } catch (error) {
+            console.error("RabbitMQ Connection Error : ", error.message);
+            retries--;
+            console.error("Retrying again: ", retries);
+            await new Promise(res => setTimeout(res,delay));
+        }
+    }
+}
 app.get('/tasks', async (req, res) => {
     const tasks = await Task.find();
     res.json(tasks);
@@ -35,6 +55,16 @@ app.post('/tasks', async (req, res) => {
     try {
         const task = new Task({ title, description, userId });
         await task.save();
+
+        const message = { taskId: task._id, userId, title };
+
+        if(!channel){
+            return res.status(503).json({error: "RabbitMQ not connected"});
+        }
+        channel.sendToQueue("task_created", Buffer.from(
+            JSON.stringify(message)
+        )) 
+
         res.status(201).json(task);
     } catch (error) {
         console.error("Error saving: ", err);
@@ -48,4 +78,5 @@ app.post('/tasks', async (req, res) => {
 app.listen(port, () => {
     // this is what gets printed on the console 
     console.log(`Task Service listening on port ${port}`)
+    connectRabbitMQWithRetry();
 })
